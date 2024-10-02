94ad74d37f5fc77cb6e67dd27e05e8cee409c4ee
Caleb Chiang <calebchiang1002@gmail.com>
2024-10-01 19:39:16 -0700

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the BitJobs server!');
